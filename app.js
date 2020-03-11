4f7cb591d500e765c120ccd2b72266648d7517a0
Josh Lear <joshrlear@gmail.com>
2020-03-10 18:07:59 -0700

    mv pdf417Decode to separate file

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 const { faceDetect, faceVerify } = require('./faceApi')
-const { ocr } = require('./textDetect')
-
+const { pdf417Decode } = require('./dlDetect')
 
 
 /* ------------for firebase Auth-------------- */
@@ -15,106 +14,12 @@ admin.initializeApp({
   databaseURL: "https://testkamshed.firebaseio.com"
 });
 
-
-
-
 /* ------------for request-------------- */
 const { multer } = require('./middleware')
 
 
-
-
 let userInfo = {} // object that holds important response data
 
-const Dynamsoft = require('dynamsoft-node-barcode')
-Dynamsoft.BarcodeReader.productKeys = 't0068MgAAACdpEm7LWt4nFOtJAWGi5VkqZQ8iBn0j9tP6RTXKh3NqBb2m7Vkt3ATDjtLhHo+vJA+N4RllCKb2oxFSdxLvTwM='
-
-
-app.post('/pdf417', multer.any(), async (req, res) => {
-
-  let dlInfo = []
-  
-  console.log("============== create reader ==============");
-  let reader = await Dynamsoft.BarcodeReader.createInstance();
-  const [ barcode ] = await req.files
-  console.log("============== decode buffer ==============");
-
-  const barcodeInfo = await reader.decode(barcode.buffer)
-  let data;
-  let identityInfo = {}
-  barcodeInfo.map(_ => {
-    if (_.barcodeFormatString === 'PDF417') { // find PDF417 barcode
-      data = _.barcodeText                    // convert str to arr
-        .replace(/\n/g, ',')
-        .split(',')
-        .slice(2)                             // drop unused vals
-    }
-  })
-  data.map((_, i) => {
-    
-    switch (_.slice(0,3)) {
-      case 'DAC':
-        identityInfo.fName = data[i].slice(3)
-        break
-      case 'DCS':
-        identityInfo.lName = data[i].slice(3)
-        break
-      case 'DAD':
-        identityInfo.mInitial = data[i].slice(3)
-        break
-      case 'DBB':
-        identityInfo.dob = data[i].slice(3)
-        break
-      case 'DAU':
-        identityInfo.height = data[i].substr(3,3)
-        break
-      case 'DBC':
-        console.log('gender:', data[i].slice(3))
-        switch (data[i].slice(3)) {
-          case '1': 
-            identityInfo.gender = 'male'
-            break
-          case '2':
-            identityInfo.gender = 'female'
-            break
-          case '9':
-            identityInfo.gender = 'not specified'
-            break
-        }
-        break
-      case 'DAG':
-        identityInfo.streetAddress = data[i].slice(3)
-        break
-      case 'DAI':
-        identityInfo.city = data[i].slice(3)
-        break
-      case 'DAJ':
-        identityInfo.state = data[i].slice(3)
-        break
-      case 'DAK':
-        identityInfo.zipCode = data[i].substr(3,5)
-        break
-      case 'DCG':
-        identityInfo.county = data[i].slice(3)
-        break
-      case 'DDB':
-        identityInfo.lastModified = data[i].slice(3)
-        break
-      case 'DBA':
-        identityInfo.expireDate = data[i].slice(3)
-        break
-    }
-  })
-  res.send(identityInfo)
- 
-  console.log("============== destroy reader ==============");
-  reader.destroy();
-  process.exit();
-
-})
-
-
-/* ------------request start-------------- */
 app.post('/upload', multer.any(), async (req, res, next) => {
 
   if (!req.files || !req.body.userId) {
@@ -125,32 +30,28 @@ app.post('/upload', multer.any(), async (req, res, next) => {
   const { userId } = req.body
   userInfo.userId = userId
 
-  const [ idImage, userImage ] = req.files
-  const unverifiedImages = [idImage.buffer, userImage.buffer]
-
+  const [ idFront, idBack, userImage ] = req.files
   let faceIds = []
   
-  const test = Promise.all([
-      unverifiedImages.map(image => {
+  
+  Promise.all([
+    
+      [idFront.buffer, userImage.buffer].map(image => {
       Promise.all([
-        faceDetect(image, faceIds)
+        faceDetect(image, faceIds) // facial recognition
         .then(result => {
           if (result) {
             const p = Promise.resolve(faceVerify(result))
             p.then(response => {
               userInfo.faceVerification = response
-              res.send(userInfo)
             })
           }
         })
       ])
     }),
-    userInfo.idText = await ocr(idImage.buffer)
+    userInfo.idText = await pdf417Decode(idBack.buffer), // PDF417 info extraction
+    res.send(userInfo)
   ])
-  //test.then(() => res.send(userInfo))
-}, () => {
-  console.log(userInfo)
-  return userInfo
 })
 
 module.exports = {
--- dlDetect.js
+++ dlDetect.js
@@ -1,40 +1,87 @@
 let Dynamsoft = require('dynamsoft-node-barcode');
-// Please visit https://www.dynamsoft.com/CustomerPortal/Portal/TrialLicense.aspx to get trial license.
 Dynamsoft.BarcodeReader.productKeys = 't0068MgAAACdpEm7LWt4nFOtJAWGi5VkqZQ8iBn0j9tP6RTXKh3NqBb2m7Vkt3ATDjtLhHo+vJA+N4RllCKb2oxFSdxLvTwM=';
 
 
-const dlDecode = async buffer => {
+const pdf417Decode = async buffer => {
     console.log("============== create reader ==============");
-
     let reader = await Dynamsoft.BarcodeReader.createInstance();
-
+    //const [ barcode ] = await req.files
     console.log("============== decode buffer ==============");
+  
+    const barcodeInfo = await reader.decode(buffer)
+    let data;
+    let identityInfo = {}
+    barcodeInfo.map(_ => {
+      if (_.barcodeFormatString === 'PDF417') { // find PDF417 barcode
+        data = _.barcodeText                    // convert str to arr
+          .replace(/\n/g, ',')
+          .split(',')
+          .slice(2)                             // drop unused vals
+      }
+    })
+    data.map((_, i) => {
+      
+      switch (_.slice(0,3)) {
+        case 'DAC':
+          identityInfo.fName = data[i].slice(3)
+          break
+        case 'DCS':
+          identityInfo.lName = data[i].slice(3)
+          break
+        case 'DAD':
+          identityInfo.mInitial = data[i].slice(3)
+          break
+        case 'DBB':
+          identityInfo.dob = data[i].slice(3)
+          break
+        case 'DAU':
+          identityInfo.height = data[i].substr(3,3)
+          break
+        case 'DBC':
+          console.log('gender:', data[i].slice(3))
+          switch (data[i].slice(3)) {
+            case '1': 
+              identityInfo.gender = 'male'
+              break
+            case '2':
+              identityInfo.gender = 'female'
+              break
+            case '9':
+              identityInfo.gender = 'not specified'
+              break
+          }
+          break
+        case 'DAG':
+          identityInfo.streetAddress = data[i].slice(3)
+          break
+        case 'DAI':
+          identityInfo.city = data[i].slice(3)
+          break
+        case 'DAJ':
+          identityInfo.state = data[i].slice(3)
+          break
+        case 'DAK':
+          identityInfo.zipCode = data[i].substr(3,5)
+          break
+        case 'DCG':
+          identityInfo.county = data[i].slice(3)
+          break
+        case 'DDB':
+          identityInfo.lastModified = data[i].slice(3)
+          break
+        case 'DBA':
+          identityInfo.expireDate = data[i].slice(3)
+          break
+      }
+    })
 
-    //let fs = require('fs');
-    //let buffer = fs.readFileSync(__dirname + '/joshId.jpg');
-    for(let result of await reader.decode(buffer)){
-        /* 
-            info we want:  maybe(?)
-            - fName
-            - lName
-            - mInitial
-            - DOB
-            - DL#
-            - Street Address (?)
-            - City (?)
-            - State (?)
-            - Postal [zip] code (?)
-            - Gender (?)
-        */
-        console.log(result.barcodeText);
-    }
-    // cross-ref data with publicData.com
     console.log("============== destroy reader ==============");
-    
     reader.destroy();
-    process.exit();
+    return identityInfo
+   
+    //process.exit();
 }
 
 module.exports = {
-    dlDecode
+    pdf417Decode
 }
